a34c9618cfa090ea14f9da42855bcd019a65add8
Ismael <isma97.rasocity@gmail.com>
2025-09-15 22:14:56 +0200

    Handling of invalid inputs

--- src/endpoints/get_character_data.py
+++ src/endpoints/get_character_data.py
@@ -1,12 +1,20 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, HTTPException, status, Query
+from pydantic import BaseModel
 from operator import attrgetter
 
 from src.rm_api import Character_Data, get_data
 
+class ErrorResponse(BaseModel):
+    detail: str
 
 character_router = APIRouter()
 
-@character_router.get("/earth_characters")
+@character_router.get("/earth_characters", responses={
+    400: {
+        "model": ErrorResponse,
+        "description": "Invalid user input"
+    }
+})
 async def get_earth_characters(sort_by: list[str] | None = Query(None, description=f"Allowed values: {', '.join(list(Character_Data.model_fields.keys()))}")) -> list[Character_Data]:
     
     earth_characters = await get_data()
@@ -16,7 +24,7 @@ async def get_earth_characters(sort_by: list[str] | None = Query(None, descripti
     
     for field in sort_by:
         if field not in Character_Data.model_fields:
-            raise ValueError(f"Invalid sort field: {field}")
+            raise HTTPException(detail=f"Invalid sort field: {field}", status_code=status.HTTP_400_BAD_REQUEST)
     
     sorted_characters = sorted(
         earth_characters,
